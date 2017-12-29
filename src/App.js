4cec129ab1416c4e7ee7a508b644de1763f0669f
Fujiwara <serko_x@uke.net>
2017-12-29 17:57:42 +0200

    USD and EUR

--- src/App.js
+++ src/App.js
@@ -7,38 +7,50 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {res: []};
-    this.url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+    this.url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10&convert=EUR'
     this.request = require('request')
 
     this.getAPI = this.getAPI.bind(this);
-    }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.getAPI();
+    setInterval(() => {
       this.getAPI();
-    }
+    }, 11000);
+  }
 
   getAPI () {
-  this.request.get(this.url,(error, response, body) => {
+    this.request.get(this.url,(error, response, body) => {
       this.setState({res: JSON.parse(body)});
       console.log(this.state.res);
-  })
+    })
 
-}
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
 
 
 
-        </header>
-        {this.state.res.map(function(item, i){
-        return(<p key={i} className="App-intro">
-          {item.name} : {item.price_usd} USD
-        </p>)
-    })}
+      </header>
+      {this.state.res.map(function(item, i){
+        return(
+          <div key={i} className="App-intro">
+          <div className="ga-inline-block">
+          {item.name}
+          </div>
+          <div className="ga-inline-block">
+          {parseFloat(item.price_usd).toFixed(2) } USD
+          </div>
+          <div className="ga-inline-block">
+          {parseFloat(item.price_eur).toFixed(2) } EUR
+          </div>
+        </div>)
+      })}
       </div>
     );
   }
